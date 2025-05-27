ba3257f3990178f57c10cd7e24fd7fe2e34a4022
angela-valeria <angela.valeria@ug.uchile.cl>
2025-05-27 16:49:42 -0400

    cambios de orden principalmente

--- procesamiento/01proc_data.R
+++ procesamiento/01proc_data.R
@@ -4,9 +4,9 @@
 #                     Facultad de Ciencias Sociales
 #                       Análisis multinivel 2025
 #
-#                Cahuil Ortiz, Juan Prado y Ángela Valeria
+#                Cahuil Ortiz, Juan Prado y Angela Valeria
 #                     Profesor: Juan Carlos Castillo
-#                         Apoyo docente: Kevin
+#                    Apoyo docente: Kevin Carrasco
 #                  Fecha de entrega: 28 de mayo de 2025
 #
 #                        Plantilla procesamiento
@@ -32,32 +32,127 @@ pacman::p_load(tidyverse, # para sintaxis
                reghelper,
                texreg)
             
-options(scipen = 999) # para desactivar notacion cientifica
+options(scipen = 999) # para desactivar notación científica
 rm(list = ls())       # para limpiar el entorno de trabajo
 
 ## Carga de datos ------------------------------------------
 
-# Carga datos SÓLO 1ERA VEZ (para subir la base original al github)
+# Carga datos *SÓLO 1ERA VEZ* (para subir la base original al github)
 
 load("input/data/Latinobarometro_2023_Esp_Rdata_v1_0.rdata")
 
-# Carga datos base limpia, con los índices y las variables que lo componen 
+# Carga datos base limpia, con el índice y las variables que lo componen 
 
 latbar2023 <- readRDS("output/latbar2023.RDS")
 
-# Carga datos base limpia, sólo con los índices, no con los que lo componen****
+# Carga datos base limpia, sólo con el índice, no con las variables
+# que lo componen****
 
 latbar2023_final <- readRDS("output/latbar2023_final.RDS")
 
-# Carga datos base agregada****
+# Carga datos base anterior agregada****
 
-latbar2023_final_agg <- readRDS("output/latbar2023_final_agg.RDS")
+agg_latbar2023_final <- readRDS("output/agg_latbar2023_final.RDS")
+
+# **** significa que se abren estas para los análisis
+
+## Selección variables de interés ----------------------------------
+
+latbar2023 <- Latinobarometro_2023_Esp_v1_0 %>%
+  select(S2, P16ST,P11STGBS.B, P61ST, P32INN, 
+         idenpa, P41ST.A, P41ST.B, P41ST.C,
+         P41ST.D, P41ST.E, P41ST.F, P41ST.G, 
+         P41ST.H, P41ST.I, P41ST.J, P41ST.K,
+         P41ST.L, P41ST.M) %>%
+  as.data.frame()
 
 # Eliminar datos perdidos------------------------------------------------------
 
 latbar2023 <- latbar2023 %>%
   mutate(across(everything(), ~ replace(., . %in% c(-5, -2, -3, -1, 97), NA)))
 
-latbar2023_final=na.omit(latbar2023) #Sacar missing data
-names(latbar2023_final)
-summary(latbar2023_final)
+latbar2023=na.omit(latbar2023) #Sacar missing data
+
+names(latbar2023)
+summary(latbar2023)
+
+# Recodificación valores para índice y construcción del índice
+
+latbar2023 <- mutate(latbar2023, P41ST.A = car::recode(latbar2023$P41ST.A, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.B = car::recode(latbar2023$P41ST.B, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.C = car::recode(latbar2023$P41ST.C, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.D = car::recode(latbar2023$P41ST.D, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.E = car::recode(latbar2023$P41ST.E, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.F = car::recode(latbar2023$P41ST.F, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.G = car::recode(latbar2023$P41ST.G, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.H = car::recode(latbar2023$P41ST.H, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.I = car::recode(latbar2023$P41ST.I, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.J = car::recode(latbar2023$P41ST.J, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.K = car::recode(latbar2023$P41ST.K, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.L = car::recode(latbar2023$P41ST.L, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+latbar2023 <- mutate(latbar2023, P41ST.M = car::recode(latbar2023$P41ST.M, 
+                                                       "1=3; 2=2; 3=1; 4=0"))
+
+latbar2023 <- mutate(latbar2023, garantias_pais = (P41ST.A + P41ST.B + P41ST.C + 
+                                                   P41ST.D + P41ST.E + P41ST.F + 
+                                                   P41ST.G + P41ST.H + P41ST.I + 
+                                                   P41ST.J + P41ST.K + P41ST.L + 
+                                                   P41ST.M)/13)
+
+# Otras recodificaciones
+
+latbar2023 <- mutate(latbar2023, S2 = car::recode(latbar2023$S2, "1=4; 2=3; 3=2;
+                                                                  4=1; 5=0"))
+
+latbar2023 <- mutate(latbar2023, P11STGBS.B = car::recode(latbar2023$P11STGBS.B,
+                                                          "1=3; 2=2; 3=1; 4=0"))
+
+latbar2023 <- mutate(latbar2023, P61ST = car::recode(latbar2023_final$P61ST,
+                           "1=0; 2=1; 3=2; 4=3; 5=4; 6=5; 7=6; 8=7; 9=8; 10=9"))
+
+latbar2023 <- mutate(latbar2023, P32INN = car::recode(latbar2023$P32INN,
+                                                      "1=2; 2=0; 3=1"))
+
+# Creación base con el índice, excluyendo las variables que lo componen
+
+latbar2023_final <- latbar2023 %>% select(clase_scl = S2, 
+                                          tend_politica = P16ST,
+                                          perc_economia = P11STGBS.B, 
+                                          perc_desigualdad = P61ST, 
+                                          perc_libertad = P41ST.A, 
+                                          perc_migracion = P32INN,
+                                          pais = idenpa, 
+                                          garantias_pais = garantias_pais)
+
+# Etiquetar valores
+
+latbar2023_final$pais <- factor(latbar2023_final$pais, levels = 
+           c(32,68,76,170,188,152,218,222,320,340,484,591,600,604,858,862,214), 
+           labels = c("Argentina","Bolivia", "Brasil", "Colombia", "Costa Rica", 
+                      "Chile", "Ecuador", "El Salvador", "Guatemala", 
+                      "Honduras", "México", "Panamá", "Paraguay", "Perú", 
+                      "Uruguay", "Venezuela", "República Dominicana"))
+
+# Creación base agregada por país
+
+agg_latbar2023_final=latbar2023_final %>% group_by(pais) %>% 
+  summarise_all(funs(mean)) %>% as.data.frame()
+
+## Guardar datos --------------------------------------------------------------
+
+save(data,file="output/data.RData")
+saveRDS(latbar2023, file = "output/latbar2023.RDS")
+saveRDS(latbar2023_final, file = "output/latbar2023_final.RDS")
+saveRDS(agg_latbar2023_final, file = "output/agg_latbar2023_final.RDS")
--- procesamiento/02analisis.R
+++ procesamiento/02analisis.R
@@ -1,115 +1,25 @@
 
 ## Exploración y descripción--------------------------------------------------
 
-dim(latbar2023) # dimensiones de base de datos
+dim(latbar2023_final) # dimensiones de base de datos
 
-names(latbar2023) # Muestra los nombres de las variables en la base
+names(latbar2023_final) # Muestra los nombres de las variables en la base
 
-# Selección variables de interés
+dim(latbar2023_final) # dimensiones
 
-latbar2023 <- Latinobarometro_2023_Esp_v1_0 %>%
-  select(S2, edad, P16ST,
-         P11STGBS.B, P61ST, P32INN, idenpa,P41ST.A, P41ST.B, P41ST.C,
-         P41ST.D, P41ST.E, P41ST.F, P41ST.G, P41ST.H, P41ST.I, P41ST.J, P41ST.K,
-         P41ST.L, P41ST.M) %>%
-  as.data.frame()
+head(latbar2023_final) # primeros 10 casos cada variable seleccionada
 
-dim(latbar2023) # dimensiones
-
-head(latbar2023) # primeros 10 casos cada variable seleccionada
-
-summary(latbar2023) # Descriptivos generales (evaluación de datos perdidos)
+summary(latbar2023_final) # Descriptivos generales (evaluación de datos perdidos)
 
 # Tabla descriptiva con stargazer
 
-stargazer(latbar2023, title = "Descriptivos generales", type='html')
-
-# Recodificación valores para índice y construcción del índice
-
-latbar2023_final <- mutate(latbar2023_final, P41ST.A = car::recode(latbar2023_final$P41ST.A, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.B = car::recode(latbar2023_final$P41ST.B, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.C = car::recode(latbar2023_final$P41ST.C, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.D = car::recode(latbar2023_final$P41ST.D, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.E = car::recode(latbar2023_final$P41ST.E, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.F = car::recode(latbar2023_final$P41ST.F, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.G = car::recode(latbar2023_final$P41ST.G, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.H = car::recode(latbar2023_final$P41ST.H, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.I = car::recode(latbar2023_final$P41ST.I, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.J = car::recode(latbar2023_final$P41ST.J, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.K = car::recode(latbar2023_final$P41ST.K, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.L = car::recode(latbar2023_final$P41ST.L, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-latbar2023_final <- mutate(latbar2023_final, P41ST.M = car::recode(latbar2023_final$P41ST.M, 
-                                                                   "1=3; 2=2; 3=1; 4=0"))
-
-latbar2023_final <- mutate(latbar2023_final, garantias_pais = (P41ST.A + P41ST.B + P41ST.C + 
-                                                                 P41ST.D + P41ST.E + P41ST.F + P41ST.G + 
-                                                                 P41ST.H + P41ST.I + P41ST.J + P41ST.K + 
-                                                                 P41ST.L + P41ST.M)/13)
-
-# Otras recodificaciones
-
-latbar2023_final <- mutate(latbar2023_final, S2 = car::recode(latbar2023_final$S2, "1=4; 2=3; 3=2;
-                                                  4=1; 5=0; -1=4; -2=3"))
-
-latbar2023_final <- mutate(latbar2023_final, P16ST = car::recode(latbar2023_final$P16ST, "-1=1; -2=2"))
-
-latbar2023_final <- mutate(latbar2023_final, P11STGBS.B = car::recode(latbar2023_final$P11STGBS.B,
-                                                                      "1=3; 2=2; 3=1; 4=0; -1=3; -2=2"))
-
-latbar2023_final <- mutate(latbar2023_final, P61ST = car::recode(latbar2023_final$P61ST,
-                                                                 "1=0; 2=1; 3=2; 4=3; 5=4; 6=5; 7=6; 8=7; 9=8; 10=9; -1=0; -2=1"))
-
-latbar2023_final <- mutate(latbar2023_final, P32INN = car::recode(latbar2023_final$P32INN,
-                                                                  "1=2; 2=0; 3=1"))
-
-# Creación base con el índice,
-# excluyendo las variables que lo componen
-
-latbar2023_final <- latbar2023_final %>% select(clase_scl = S2, tend_politica = P16ST,
-                                                perc_economia = P11STGBS.B, perc_desigualdad = P61ST, 
-                                                perc_libertad = P41ST.A, perc_migracion = P32INN,
-                                                pais = idenpa, edad = edad, 
-                                                garantias_pais = garantias_pais)
-
-# Etiquetar valores
-
-latbar2023_final$pais <- factor(latbar2023_final$pais, levels = 
-                                  c(32,68,76,170,188,152,218,222,320,340,484,591,600,604,858,862,214), 
-                                labels = c("Argentina","Bolivia", "Brasil", "Colombia", "Costa Rica", 
-                                           "Chile", "Ecuador", "El Salvador", "Guatemala", "Honduras",
-                                           "México", "Panamá", "Paraguay", "Perú", "Uruguay", 
-                                           "Venezuela", "República Dominicana"))
-
-# Creacion variable promedio de garantías país
-
-latbar2023_final_agg=latbar2023_final %>% group_by(pais) %>% 
-  summarise_all(funs(mean)) %>% as.data.frame()
+stargazer(latbar2023_final, title = "Descriptivos generales", type='html')
 
 stargazer(latbar2023_final_agg, type = "text")
-
-## Descriptivos generales nueva base ----------------------------------------------------
-
-stargazer(latbar2023_final, title = "Descriptivos generales", type='html')
 
 # Revisión estadísticos descriptivos cada variable
 
 freq(latbar2023_final$clase_scl)
-
-descr(latbar2023_final$edad, stats = 
-        c("min", "med", "max", "mean", "sd"), transpose = T)
-freq(latbar2023_final$edad)
 
 descr(latbar2023_final$tend_politica, stats = 
         c("min", "q1", "med", "q3", "max", "mean", "sd"), transpose = T)
@@ -200,8 +110,7 @@ screenreg(resultados_4)
 
 # Regersión comparación
 
-reg_ind=lm(tend_politica ~ edad + clase_scl + perc_economia + perc_libertad + perc_desigualdad, data=latbar2023_final)
-agg_latbar2023_final=latbar2023_final %>% group_by(pais) %>% summarise_all(funs(mean))
+reg_ind=lm(tend_politica ~ clase_scl + perc_economia + perc_libertad + perc_desigualdad, data=latbar2023_final)
 
 reg_agg=lm(tend_politica ~ edad + clase_scl + perc_economia + perc_libertad + perc_desigualdad, data=agg_latbar2023_final)
 
@@ -229,12 +138,3 @@ htmlreg(list(reg_ind, reg_agg, resultados_3),
         caption="Primera comparación de modelos Individual, Agregado y Multinivel",
         caption.above=TRUE,
         doctype = FALSE)
-
-
-## Guardar datos --------------------------------------------------------------
-
-save(data,file="output/data.RData")
-saveRDS(latbar2023, file = "output/latbar2023.RDS")
-saveRDS(latbar2023_final, file = "output/latbar2023_final.RDS")
-saveRDS(latbar2023_final_agg, file = "output/latbar2023_final_agg.RDS")
-
